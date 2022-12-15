971af0ecc3edf67140392cc7609c0732084cd957
shamil <shamik230@gmail.com>
2022-12-15 23:35:38 +0300

    media root static root

--- landing/urls.py
+++ landing/urls.py
@@ -16,9 +16,11 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path
 from crm import views
+from django.conf import settings
+from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', views.index, name='index'),
     path('thanks/', views.thanks, name='thanks')
-]
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
